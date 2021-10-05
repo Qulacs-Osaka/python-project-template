fc8c4c2aa74fd239bd5d4ad9f86140200be6cccc
ikanago <ikanago-dev@protonmail.com>
2021-10-05 15:16:48 +0900

    Function to rename

--- rename.sh
+++ rename.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -ex
 
+# $1: Project name which is renamed to.
 function validate_project_name() {
-    local pattern=" "
-    if [[ $1 =~ $pattern ]]
+    local pattern="[a-z]|_|"
+    if [[ ! $1 =~ $pattern ]]
     then
-        echo "Project name must not contain space."
+        echo "Project name must consists of a-z and underscore(_)."
         exit 1
     fi
 }
 
 function rename_project_in_file() {
-    echo $1
+    local platform="win"
+    case $platform in
+        "linux-gnu"*) sed -i 's/$1/$2/g' $3 ;;
+        "darwin"*) sed -i"" 's/$1/$2/g' $3 ;;
+        *) echo "Unsupported platform: $platform"; exit 1 ;;
+    esac
 }
 export -f rename_project_in_file
 
+# Rename project name in files.
+# $1: Project name which is renamed from.
+# $2: Project name which is renamed to.
 function rename_project() {
     local files=$(find . \
     -not \( -path "./.venv" -prune \) \
@@ -23,11 +32,11 @@ function rename_project() {
     -not \( -path "./.mypy_cache" -prune \) \
     -type f)
     for file in $files; do
-        rename_project_in_file $file
+        rename_project_in_file $1 $2 $file
     done
 }
 
 RENAME_FROM="project-template"
 RENAME_TO=$1
 validate_project_name "$RENAME_TO"
-rename_project
+rename_project "$RENAME_FROM" "$RENAME_TO"
